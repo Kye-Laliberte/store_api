acaa878af5e04dffcc19ba3bcdef73a408965364
Kye Laliberte <klaliber@icloud.com>
2026-03-02 19:33:22 -0700

    set up connect and file structer

---
+++ api/connect.py
@@ -0,0 +1,33 @@
+import os
+
+import psycopg2
+from contextlib import contextmanager
+from dotenv import load_dotenv
+from pathlib import Path
+
+
+
+@contextmanager
+def get_connection():
+
+    dotenv_path = Path("C:\projects\vs stuff 2\New folder (2)\store-interface/.env")
+    load_dotenv(dotenv_path=dotenv_path)
+    
+
+    conn= psycopg2.connect(
+        dbname=os.getenv("POSTGRES_DB"),
+        user=os.getenv("POSTGRES_USER"),
+        password=os.getenv("POSTGRES_PASSWORD"),
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT")
+        )
+    
+
+    try:
+        yield conn
+        conn.commit()
+    except:
+        conn.rollback()
+        raise
+    finally:
+        conn.close()
