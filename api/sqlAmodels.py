664701e059b06d665ba30fff4caada4bc74a5be7
Kye Laliberte <klaliber@icloud.com>
2026-04-05 13:09:21 -0600

    fixed a mispeling in the reqierments.txt created a second file for the order models

--- api/sqlAmodels.py
+++ api/sqlAmodels.py
@@ -7,27 +7,8 @@ from datetime import datetime
 __table_args__ = (
     CheckConstraint('quantity > 0'),
     CheckConstraint('price > 0'),
-    CheckConstraint('total_price >= 0'),
     CheckConstraint('quantity_available >= 0'))
 Base = declarative_base()
-class Order(Base):
-    __tablename__ = "orders"
-    id = Column(Integer, primary_key=True, index=True)
-    total_price = Column(Numeric(10, 2), CheckConstraint('total_price >= 0'), nullable=False)
-    user_id =Column(Integer, ForeignKey("users.id"), nullable=False)
-    order_date = Column(DateTime, default=datetime.utcnow)
-    
-    user = relationship("User", back_populates="orders")
-    order_items = relationship("OrderItem", back_populates="order", cascade="all, delete-orphan")
-
-class OrderItem(Base):
-    __tablename__ = "order_items"
-    order_id = Column(Integer, ForeignKey("orders.id"), primary_key=True)
-    item_id = Column(Integer, ForeignKey("items.id"), primary_key=True)
-    quantity = Column(Integer, nullable=False)
-
-    order = relationship("Order", back_populates="order_items")
-    item = relationship("Item")
 
 class User(Base):
     __tablename__ = "users"
