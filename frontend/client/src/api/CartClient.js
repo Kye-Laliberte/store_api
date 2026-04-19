013f2f03fc71140453fd9cb38132a113f079102b
Kye Laliberte <klaliber@icloud.com>
2026-04-19 16:14:52 -0600

    Fix addToCart function and set up viewCart.js function with error handling

--- frontend/client/src/api/CartClient.js
+++ frontend/client/src/api/CartClient.js
@@ -1,3 +1,5 @@
+import { cache } from "react";
+
 const BASE_URL = "http://localhost:8000";
 const Cart_URL = `${BASE_URL}/carts`;
 
@@ -13,9 +15,7 @@ export async function addToCart(user_id,item_id,quantity) {
                 body: JSON.stringify({             
                     item_id,
                     quantity
-                })
-            }
-        );
+                })});
     if (!response.ok) {
       throw new Error("failed request");
     }
@@ -24,6 +24,30 @@ export async function addToCart(user_id,item_id,quantity) {
         console.error("error adding item to cart ", err)
         throw err;
     }
+}    
+export async function viewCart(user_id) {
+    try{
+        const respon= await fetch(
+            `${Cart_URL}/${user_id}/viewcart`,{
+                method: "get",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    item_id,
+                    name,
+                    price,
+                    quantity,
+                    description
+                })});
+    if (!respon.ok) {
+      throw new Error("failed request");
+    }
+    return await respon.json();    
+    }catch(err){
+        console.error("failed to find cart")
+    }
     
+}
    
-} 
+ 
