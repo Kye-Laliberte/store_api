6d3817c35e20e5085e9c26ad39ea2118cc17668a
Kye Laliberte <klaliber@icloud.com>
2026-05-05 17:13:07 -0500

    Add initial migration script to update cart purchase date to cart_date

---
+++ alembic/versions/2db0d8895f5d_updated_cart_purchis_date_is_now_cart_.py
@@ -0,0 +1,24 @@
+"""updated Cart: purchis date is now cart_date
+
+Revision ID: 2db0d8895f5d
+Revises: 785afc63a6c4
+Create Date: 2026-05-05 17:05:24.612016
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers
+revision = '2db0d8895f5d'
+down_revision = '785afc63a6c4'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
