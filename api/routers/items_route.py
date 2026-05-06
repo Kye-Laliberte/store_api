ad1e275ebd5b5149516d3b398ba816db5b094182
Kye Laliberte <klaliber@icloud.com>
2026-05-06 16:13:30 -0500

    fixed a bug in readAll Items and stremlind its output with  config.

--- api/routers/items_route.py
+++ api/routers/items_route.py
@@ -2,72 +2,71 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from api.database import  get_db
 import api.models.sqlAmodels as models
-from api.psycopg_models import item,createitem, updateitem
+from api.psycopg_models import item,createitem, updateitem, ItemSchema
 from typing import List, Optional
 from sqlalchemy import text
 
 router = APIRouter(prefix="/items", tags=["items"])
 
 # READ all items
-@router.get("/get_all")
-def readAllItems(db: Session = Depends(get_db),response_model =List[item]):
-    return db.query(models.Item).filter(models.Item.quantity>0).all()
+@router.get("/get_all",response_model = List[ItemSchema])
+def readAllItems(db: Session = Depends(get_db)):
+    itemlist= db.query(models.Item).filter(models.Item.quantity > 0).all()
+
+    return itemlist
 
 
 @router.post("/add_item",response_model=item)
-def create_item(item:createitem, db: Session = Depends(get_db)):
+def create_item(items:createitem, db: Session = Depends(get_db)):
     """add a item to the stores inventory"""
-    name=item.name.strip().lower()
-    description=item.description.strip()
-    if item.price<0:
-        raise HTTPException(status_code= 200, detail="can not have a 0 or negative price" )
-    if item.quantity<0:
-        raise HTTPException(status_code=200, detail="can not have a negative inventory")
+    name=items.name.strip().lower().strip()
+    description=items.description.strip()
+
     existing = db.query(models.Item).filter(models.Item.name == name).first()
     if existing:
         raise HTTPException(status_code=400, detail="Item already exists")
     try:
-         item = models.Item(name=name, description=description, quantity=item.quantity, price=item.price)
-         db.add(item)
+         out = item(name=name, description=description, quantity=item.quantity, price=item.price)
+         db.add(out)
          db.commit()
-         db.refresh(item)
+         db.refresh(out)
     except Exception as e:
         db.rollback()
         raise HTTPException(status_code=500, detail="Error creating item") from e
-    return item
+    return out
 
 # UPDATE an item
 @router.put("/{item_id}/update",response_model=item)
 def update_item(item_id: int,update:updateitem, db: Session = Depends(get_db)):
     """update a items infermation"""
-    item = db.query(models.Item).filter(models.Item.id == item_id).first()
-    if not item:
+    items = db.query(models.Item).filter(models.Item.id == item_id).first()
+    if not items:
         raise HTTPException(status_code=404, detail="Item not found")
     try:
-        if update.quantity is not None and update.quantity>=0:
-            item.quantity = update.quantity
-        if update.price is not None and update.price>0:
-            item.price = update.price
-        if update.description is not None:    
-            item.description=update.description
+        if update.quantity != None:
+            items.quantity = update.quantity
+        if update.price:
+            items.price = update.price
+        if update.description:    
+            items.description=update.description
         db.commit()
-        db.refresh(item)
+        db.refresh(items)
+        return item(id=items.id,name=items.name, description=items.description, quantity=items.quantity, price=items.price)
     except Exception as e:
         db.rollback()
-        raise HTTPException(status_code=500, detail="Error updating item") from e
+        raise HTTPException(status_code=500, detail=f"Error updating item {e}") from e
     
-    return item
 
 #get items details
 @router.get("/{item_id}/details",response_model=item)
 def getItem(item_id: int, db: Session = Depends(get_db)):
     """gets items infermation"""
     
-    item=db.query(models.Item).filter(models.Item.id ==item_id).first()
-    
-    if not item:
+    items=db.query(models.Item).filter(models.Item.id ==item_id).first()
+    if not items:
         raise HTTPException(status_code=404, detail="Item not found")
-    return item
+    out=item(name=items.name, description=items.description, quantity=items.quantity, price=items.price)
+    return out
 
 
 #@router.put("/{item_id}/Hold",response_model=item)
