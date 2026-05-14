d599f7495f2c0cc41cc0186c930aff4190da0263
Kye Laliberte <klaliber@icloud.com>
2026-05-14 13:21:41 -0500

    add basic error cach for testing, and added removeItemFromCart().js fo fucher use

--- frontend/client/src/api/CartClient.js
+++ frontend/client/src/api/CartClient.js
@@ -9,8 +9,10 @@ export async function addToCart(user_id,item_id,quantity) {
         const out= await api.post(
             `carts/${user_id}/additem`,
             {item_id:item_id,quantity:quantity});
-    
-    return await out.json();
+        if(out.status !== 200){
+            throw new Error(`Failed to add item to cart: ${out.statusText}`);
+        }
+    return out.data;
     }catch(err){
         console.error("error adding item to cart ", err)
         throw err;
@@ -20,9 +22,10 @@ export async function viewCart(user_id) {
     /**not tested should return a list[] of Items object in user_ids cart*/
     try{
         const respon= await api.get(`/carts/${user_id}/viewcart`);
-        
+        if(respon.status !== 200){
+            throw new Error(`Failed to view cart: ${respon.statusText}`);
+        }
     return respon.data;
-
     }catch(err){
         console.error("failed to find cart",err)
         throw err;
@@ -35,13 +38,30 @@ export async function addCart(user_id){
         const cart = await api.post(
             `/carts/${user_id}/newcart`,{
             id: user_id,
-            cart_date: new Date(),                   
-        });
-        
-   
-    return await cart.data;
+            cart_date: new Date()});
+    
+            if(cart.status !== 200){
+            throw new Error(`Failed to remove item from cart: ${cart.statusText}`);}
+
+            return await cart.data;
     }catch(err){
         console.error("failed to add cart", err)
         throw err;
     }
 } 
+
+export async function removeItemFromCart(user_id,item_id){
+    try{
+        const response=await api.delete(`/carts/${user_id}/removeitem`,{data:{item_id:item_id}});
+        
+        if(response.status !== 200){
+            throw new Error(`Failed to remove item from cart: ${response.statusText}`);
+        }
+        return response.data;
+    
+    }catch(err){
+        console.error("failed to remove item from cart", err)
+
+        throw err;
+    }    
+}
