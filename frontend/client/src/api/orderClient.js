2c8b17156746a60e7cdcb85ad70bf0b8059fd285
Kye Laliberte <klaliber@icloud.com>
2026-05-14 16:15:57 -0500

    fixed sintax and mising input probloms.

--- frontend/client/src/api/orderClient.js
+++ frontend/client/src/api/orderClient.js
@@ -12,10 +12,14 @@ export async function order_Cart(user_id) {
     }
 }
 
-export async function past_orders() {
+export async function past_orders(user_id) {
     try{
-    const out = await api.post(`/orders/${user_id}/vieworders`,
-        {user_id:user_id});
+    const out = await api.get(`/orders/${user_id}/vieworders`
+       );
+
+    if (out.status==204){
+        return []
+    }    
         return out.data    
     }catch(error){
         console.error("error ording your cart",error)
