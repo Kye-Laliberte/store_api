95ab45a9cbf1342ae5db9e9941bf626f30274e2b
Kye Laliberte <klaliber@icloud.com>
2026-05-14 15:57:32 -0500

    feat: add order_Cart and past_orders functions for futer UI

---
+++ frontend/client/src/api/orderClient.js
@@ -0,0 +1,25 @@
+import api from "/src/api/axios"
+
+
+export async function order_Cart(user_id) {
+    try{
+    const out = await api.post(`/orders/${user_id}/orderCart`,
+        {user_id:user_id});
+        return out.data    
+    }catch(error){
+        console.error("error ording your cart",error)
+        throw error
+    }
+}
+
+export async function past_orders() {
+    try{
+    const out = await api.post(`/orders/${user_id}/vieworders`,
+        {user_id:user_id});
+        return out.data    
+    }catch(error){
+        console.error("error ording your cart",error)
+        throw error
+    }
+
+}
