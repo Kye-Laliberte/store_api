61d90313314c16c0a147235ad67bae58dd3abcb1
Kye Laliberte <klaliber@icloud.com>
2026-05-14 16:37:23 -0500

    refactor: clean up formatting and improve error logging in order_Cart and past_orders functions

--- frontend/client/src/api/orderClient.js
+++ frontend/client/src/api/orderClient.js
@@ -3,27 +3,34 @@ import api from "/src/api/axios"
 
 export async function order_Cart(user_id) {
     try{
-    const out = await api.post(`/orders/${user_id}/orderCart`
-        );
-        return out.data    
+    const out = await api.post(`/orders/${user_id}/orderCart`);
+        return out.data;
     }catch(error){
-        console.error("error ording your cart",error)
-        throw error
+        console.error("error ording your cart",error);
+        throw error;
     }
 }
 
 export async function past_orders(user_id) {
     try{
-    const out = await api.get(`/orders/${user_id}/vieworders`
-       );
+    const out = await api.get(`/orders/${user_id}/vieworders`);
 
     if (out.status==204){
-        return []
-    }    
-        return out.data    
+        return []}    
+        return out.data;    
     }catch(error){
-        console.error("error ording your cart",error)
-        throw error
-    }
+        console.error("error ording your cart",error);
+        throw error;}
+}
 
+export async function orderDetails(order_id) {
+    try{
+        const out =await api.get(`/orders/${order_id}/details`);
+
+    return out.data;
+
+    }catch(error){
+        console.error("faild to get order detals",error);
+        throw error;
+    } 
 }
